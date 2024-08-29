99bda24d2af1e6fa0492fca52fdbdf39533f9bd9
mouse0232 <233939513@qq.com>
2024-08-29 10:30:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,88 +18,88 @@ export default defineConfig({
         }
       }
     }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: [
-          '**/*.{js,css,html,woff,woff2}',
-          '*.{js,css,html,png,svg,jpg,webp}',
-        ],
-        runtimeCaching: [
-          {
-            urlPattern: /\/(sw\.js|registerSW\.js|manifest\.webmanifest)$/, // sw 文件
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'critical-assets',
-              expiration: {
-                maxEntries: 3,
-                maxAgeSeconds: 4 * 60 * 60, // 4 小时
-              },
-            },
-          },
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|svg|webp|woff|woff2)$/, // 图片文件
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images',
-              expiration: {
-                maxEntries: 60,
-                maxAgeSeconds: 7 * 24 * 60 * 60, // 7 天
-              },
-            },
-          },
-          {
-            urlPattern: /\.(?:js|css)$/, // JS 和 CSS 文件
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'assets',
-              expiration: {
-                maxEntries: 30,
-                maxAgeSeconds: 3 * 24 * 60 * 60, // 3 天
-              },
-            },
-          },
-          {
-            urlPattern: /\/$/, // HTML
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'html',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 3 * 24 * 60 * 60, // 3 天
-              },
-            },
-          }
-        ]
-      },
-      manifest: {
-        name: 'IPCheck.ing',
-        short_name: 'IPCheck.ing',
-        theme_color: '#f8f9fa',
-        orientation: "portrait",
-        id: 'com.jasonng.myip',
-        description: 'All in one IP Toolbox',
-        icons: [
-          {
-            src: '/logos/logo-192.webp',
-            sizes: '192x192',
-            type: 'image/webp',
-            purpose: 'maskable'
-          },
-          {
-            src: '/logos/ios-logo-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/logos/ios-logo-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),    
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   injectRegister: 'auto',
+    //   workbox: {
+    //     globPatterns: [
+    //       '**/*.{js,css,html,woff,woff2}',
+    //       '*.{js,css,html,png,svg,jpg,webp}',
+    //     ],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: /\/(sw\.js|registerSW\.js|manifest\.webmanifest)$/, // sw 文件
+    //         handler: 'NetworkFirst',
+    //         options: {
+    //           cacheName: 'critical-assets',
+    //           expiration: {
+    //             maxEntries: 3,
+    //             maxAgeSeconds: 4 * 60 * 60, // 4 小时
+    //           },
+    //         },
+    //       },
+    //       {
+    //         urlPattern: /\.(?:png|jpg|jpeg|svg|webp|woff|woff2)$/, // 图片文件
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'images',
+    //           expiration: {
+    //             maxEntries: 60,
+    //             maxAgeSeconds: 7 * 24 * 60 * 60, // 7 天
+    //           },
+    //         },
+    //       },
+    //       {
+    //         urlPattern: /\.(?:js|css)$/, // JS 和 CSS 文件
+    //         handler: 'StaleWhileRevalidate',
+    //         options: {
+    //           cacheName: 'assets',
+    //           expiration: {
+    //             maxEntries: 30,
+    //             maxAgeSeconds: 3 * 24 * 60 * 60, // 3 天
+    //           },
+    //         },
+    //       },
+    //       {
+    //         urlPattern: /\/$/, // HTML
+    //         handler: 'NetworkFirst',
+    //         options: {
+    //           cacheName: 'html',
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 3 * 24 * 60 * 60, // 3 天
+    //           },
+    //         },
+    //       }
+    //     ]
+    //   },
+    //   manifest: {
+    //     name: 'IPCheck.ing',
+    //     short_name: 'IPCheck.ing',
+    //     theme_color: '#f8f9fa',
+    //     orientation: "portrait",
+    //     id: 'com.jasonng.myip',
+    //     description: 'All in one IP Toolbox',
+    //     icons: [
+    //       {
+    //         src: '/logos/logo-192.webp',
+    //         sizes: '192x192',
+    //         type: 'image/webp',
+    //         purpose: 'maskable'
+    //       },
+    //       {
+    //         src: '/logos/ios-logo-192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: '/logos/ios-logo-512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //   },
+    // }),     
     CodeInspectorPlugin({
       bundler: 'vite',
       hideDomPathAttr: true,
